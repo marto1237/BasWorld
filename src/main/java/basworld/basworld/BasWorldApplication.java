deabd03e7a69e2d8b23b07d83ae729862eed2fe5
Martin Simeonov <i525452@fhict.nl>
2024-09-30 16:02:31 +0200

    Initial setup for the api

---
+++ src/main/java/basworld/basworld/BasWorldApplication.java
@@ -0,0 +1,13 @@
+package basworld.basworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/basworld/basworld/BasWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package basworld.basworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
